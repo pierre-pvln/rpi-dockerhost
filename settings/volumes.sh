bfad7008a777098a65987610353d478944eb680a
Pierre <pierre@pvln.nl>
2017-09-11 22:20:16 +0200

    -p added

--- settings/volumes.sh
+++ settings/volumes.sh
@@ -2,7 +2,7 @@
 MY_MYSQL_DATA_VOLUME='../storage/mysql-server/data'
 if [ ! -d "$MY_MYSQL_DATA_VOLUME" ]; then
   # Control will enter here if $DIRECTORY doesn't exist.
-  mkdir $MY_MYSQL_DATA_VOLUME
+  mkdir -p $MY_MYSQL_DATA_VOLUME
 fi
 
 echo "==============="
