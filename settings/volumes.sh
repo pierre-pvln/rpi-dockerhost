49e4c8eddfe11d23ed33f37fe77a4e292e73d61a
Pierre <pierre@pvln.nl>
2017-09-12 20:43:19 +0200

    directory and ftp added

--- settings/volumes.sh
+++ settings/volumes.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-MY_MYSQL_DATA_VOLUME='../storage/mysql-server/data'
+
+MY_MYSQL_DATA_VOLUME='/home/pi/docker/ozitdatzo/storage/mysql'
 if [ ! -d "$MY_MYSQL_DATA_VOLUME" ]; then
-  # Control will enter here if $DIRECTORY doesn't exist.
+  # Will enter here if $MY_MYSQL_DATA_VOLUME doesn't exist.
   mkdir -p $MY_MYSQL_DATA_VOLUME
 fi
-
 echo "==============="
 echo "mysql data volume: " $MY_MYSQL_DATA_VOLUME
 echo "==============="
+
+MY_FTP_DATA_VOLUME='/home/pi/docker/ozitdatzo/storage/ftp'
+if [ ! -d "$MY_FTP_DATA_VOLUME" ]; then
+  # Will enter here if $MY_FTP_DATA_VOLUME doesn't exist.
+  mkdir -p $MY_FTP_DATA_VOLUME
+fi
+
+echo "==============="
+echo "mysql data volume: " $MY_FTP_DATA_VOLUME
+echo "==============="
+
